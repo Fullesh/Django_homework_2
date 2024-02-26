ccb6a60832e31ef4dff76364307db21ee17a45ba
Danila Belov <danilabelov.adm@yandex.ru>
2024-02-26 18:44:08 +0300

    Created controllers

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f"You have new feedback! Info: \n"
+              f"Subject_name: {name} \n"
+              f"Subject_phone: {phone} \n"
+              f"Subject_message: {message}")
+    return render(request, 'catalog/contacts.html')
