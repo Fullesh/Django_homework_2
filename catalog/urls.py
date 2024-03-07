b186646d6e3b499a83b1b561b735157caafce428
Danila Belov <danilabelov.adm@yandex.ru>
2024-03-07 16:43:39 +0300

    Updated app urls file

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from catalog.views import index, contacts, one_product
+from catalog.views import index, contacts, one_product, add_product
 
 urlpatterns = [
     path('', index),
     path('contacts/', contacts),
-    path('products/<int:pk>', one_product, name='product_url')
+    path('products/<int:pk>', one_product, name='product_url'),
+    path('products/add/', add_product)
 ]
