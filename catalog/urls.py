1242472f0b82bbe48e0d0f53625986bc11483db2
Danila Belov <danilabelov.adm@yandex.ru>
2024-03-19 20:13:26 +0300

    Bug fixes

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from catalog.views import index, contacts, one_product, add_product
+from catalog.views import indexListView, contacts, ProductDetailView, add_product
 
 urlpatterns = [
-    path('', index),
+    path('', indexListView.as_view()),
     path('contacts/', contacts),
-    path('products/<int:pk>', one_product, name='product_url'),
+    path('products/<int:pk>', ProductDetailView.as_view(), name='product_url'),
     path('products/add/', add_product)
 ]
