7812b3fd857a1fe5e1bdaffbcde496fc1bf0c8e2
Danila Belov <danilabelov.adm@yandex.ru>
2024-04-04 20:24:40 +0300

    Переписаны контроллеры с FBV на CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 
 from catalog.models import Product, Category
 
@@ -9,23 +9,20 @@ class indexListView(ListView):
     template_name = 'catalog/home.html'
     context_object_name = 'objects_list'
 
-# def index(request):
-#     context = {
-#         'objects_list': Product.objects.all()
-#     }
-#     return render(request, 'catalog/home.html', context)
 
+class contactsPageView(TemplateView):
+    template_name = 'catalog/contacts.html'
 
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f"You have new feedback! Info: \n"
-              f"Subject_name: {name} \n"
-              f"Subject_phone: {phone} \n"
-              f"Subject_message: {message}")
-    return render(request, 'catalog/contacts.html')
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            name = request.POST.get('name')
+            phone = request.POST.get('phone')
+            message = request.POST.get('message')
+            print(f"You have new feedback! Info: \n"
+                  f"Subject_name: {name} \n"
+                  f"Subject_phone: {phone} \n"
+                  f"Subject_message: {message}")
+        return render(request, self.template_name)
 
 
 class ProductDetailView(DetailView):
@@ -34,29 +31,28 @@ class ProductDetailView(DetailView):
     context_object_name = 'objects_list'
 
 
-# def one_product(request, pk):
-#     context = {
-#         'object': Product.objects.get(pk=pk),
-#     }
-#     return render(request, 'catalog/product.html', context)
+class ProductAddView(TemplateView):
+    template_name = 'catalog/add_product.html'
 
-
-def add_product(request):
-    context = {
-        'objects_list': Category.objects.all()
-    }
-    if request.method == 'POST':
-        product_name = request.POST.get('product_name')
-        product_description = request.POST.get('product_description')
-        price_to_buy = request.POST.get('price_to_buy')
-        product_image = request.POST.get('product_image')
-        category = request.POST.get('category')
-        info = {
-            'name': product_name,
-            'description': product_description,
-            'image': product_image,
-            'category': Category.objects.get(name=category),
-            'price_to_buy': price_to_buy
+    def get_context_data(self, **kwargs):
+        context = {
+            'objects_list': Category.objects.all()
         }
-        Product.objects.get_or_create(**info)
-    return render(request, 'catalog/add_product.html', context)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            product_name = request.POST.get('product_name')
+            product_description = request.POST.get('product_description')
+            price_to_buy = request.POST.get('price_to_buy')
+            product_image = request.POST.get('product_image')
+            category = request.POST.get('category')
+            info = {
+                'name': product_name,
+                'description': product_description,
+                'image': product_image,
+                'category': Category.objects.get(name=category),
+                'price_to_buy': price_to_buy
+            }
+            Product.objects.get_or_create(**info)
+        return render(request, self.template_name, self.get_context_data(**kwargs))
