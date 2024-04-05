617e5a0444e3cf91b1a3fdef5d8a23dfdcfdb6ef
Danila Belov <danilabelov.adm@yandex.ru>
2024-04-05 19:44:35 +0300

    Изменены настройки URL в настройках блога

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from blog.apps import BlogConfig
-from blog.views import BlogRecordCreate, BlogIndexView
+from blog.views import BlogRecordCreate, BlogRecordIndexView, BlogRecordDeatilView
 
 app_name = BlogConfig.name
 
 
 urlpatterns = [
     path('create/', BlogRecordCreate.as_view(), name='create'),
-    # path('view/', ..., name='view'),
-    path('', BlogIndexView.as_view(), name='list'),
+    path('view/<int:pk>', BlogRecordDeatilView.as_view(), name='view'),
+    path('', BlogRecordIndexView.as_view(), name='list'),
     # path('edit/<int:pk>/', ..., name='edit'),
     # path('delete/<int:pk>/', ..., name='edit'),
 
