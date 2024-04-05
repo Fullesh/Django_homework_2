48db635efdaaac8db2820b5e3e1efab8147807db
Danila Belov <danilabelov.adm@yandex.ru>
2024-04-05 09:08:07 +0300

    Добавлены контроллеры

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
+
+from blog.models import BlogRecord
+
 
 # Create your views here.
+
+class BlogRecordCreate(CreateView):
+    model = BlogRecord
+    template_name = 'blog/blog_form.html'
+    fields = ('title', 'body', 'preview', 'is_published')
+    success_url = reverse_lazy('blog:index')
+
+
+class BlogIndexView(ListView):
+    model = BlogRecord
+    template_name = 'blog/blog_list.html'
