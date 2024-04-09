6c82b4807c4ca78ac76c7d75eae44e73af0b88f1
Danila Belov <danilabelov.adm@yandex.ru>
2024-04-09 22:15:40 +0300

    Добавлен неймспейс для catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from catalog.views import indexListView, ProductDetailView, contactsPageView, ProductAddView, ProductUpdateView
 
 urlpatterns = [
-    path('', indexListView.as_view()),
+    path('', indexListView.as_view(), name='index'),
     path('contacts/', contactsPageView.as_view()),
     path('products/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
     path('products/edit/<int:pk>/', ProductUpdateView.as_view(), name='product_edit'),
