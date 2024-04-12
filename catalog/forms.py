a0ad772960d603c875c0d25b4f5ae841f476936d
Danila Belov <danilabelov.adm@yandex.ru>
2024-04-12 07:46:34 +0300

    Добавлен миксин класс для стилизации форм

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,15 +3,20 @@ from django import forms
 from catalog.models import Product, Version
 
 
-class ProductAddForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        exclude = ('updated_by',)
-
+class StyleFormMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
+            if field_name != 'actual_version_indicator':
+                field.widget.attrs['class'] = 'form-control'
+            else:
+                field.widget.attrs['class'] = 'form-check-input'
+
+
+class ProductAddForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Product
+        exclude = ('updated_by',)
 
     def clean_name(self):
         cleaned_data = self.cleaned_data.get('name')
@@ -22,7 +27,7 @@ class ProductAddForm(forms.ModelForm):
         return cleaned_data
 
 
-class VersionForm(forms.ModelForm):
+class VersionForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Version
         fields = ('product', 'version_number', 'name_of_version', 'actual_version_indicator')
