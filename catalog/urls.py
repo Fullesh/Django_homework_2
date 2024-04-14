1771f3346f9097d610e9e19f63489efb620e4f86
Danila Belov <danilabelov.adm@yandex.ru>
2024-04-14 19:45:11 +0300

    Добавлен URL удаления продукта

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from catalog.views import indexListView, ProductDetailView, contactsPageView, ProductAddView, ProductUpdateView
+from catalog.views import indexListView, ProductDetailView, contactsPageView,\
+    ProductAddView, ProductUpdateView, ProudctDeleteView
 
 urlpatterns = [
     path('', indexListView.as_view(), name='index'),
     path('contacts/', contactsPageView.as_view()),
     path('products/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
     path('products/edit/<int:pk>/', ProductUpdateView.as_view(), name='product_edit'),
-    path('products/add/', ProductAddView.as_view(), name='product_add')
+    path('products/add/', ProductAddView.as_view(), name='product_add'),
+    path('products/delete/<int:pk>/', ProudctDeleteView.as_view(), name='product_delete')
 ]
