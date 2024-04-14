deebdd8d66bf8707227b9e1a6896c5c47438d334
Danila Belov <danilabelov.adm@yandex.ru>
2024-04-14 19:44:38 +0300

    Исправлен баг в URL'ах блогового приложения

--- blog/urls.py
+++ blog/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('view/<int:pk>/', BlogRecordDeatilView.as_view(), name='view'),
     path('', BlogRecordIndexView.as_view(), name='list'),
     path('edit/<int:pk>/', BlogRecordUpdate.as_view(), name='edit'),
-    path('delete/<int:pk>/', BlogRecordDeleteView.as_view(), name='edit'),
+    path('delete/<int:pk>/', BlogRecordDeleteView.as_view(), name='delete'),
     path('published/<int:pk>/', toggle_published, name='is_published'),
 ]
