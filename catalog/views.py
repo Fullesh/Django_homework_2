a5caeb0589a14a1ce35f0dc1be681f999eb1200d
Danila Belov <danilabelov.adm@yandex.ru>
2024-04-17 21:02:43 +0300

    Добавлены права доступа в приложении "каталог"

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.forms import inlineformset_factory
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -7,7 +9,7 @@ from catalog.forms import ProductAddForm, VersionForm
 from catalog.models import Product, Version
 
 
-class indexListView(ListView):
+class indexListView(ListView, LoginRequiredMixin):
     model = Product
     template_name = 'catalog/home.html'
     context_object_name = 'objects_list'
@@ -28,24 +30,35 @@ class contactsPageView(TemplateView):
         return render(request, self.template_name)
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(DetailView, LoginRequiredMixin):
     model = Product
     template_name = 'catalog/product.html'
     context_object_name = 'objects_list'
 
 
-class ProductAddView(CreateView):
+class ProductAddView(CreateView, LoginRequiredMixin):
     model = Product
     template_name = 'catalog/add_product.html'
     form_class = ProductAddForm
     success_url = reverse_lazy('catalog:index')
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(UpdateView, LoginRequiredMixin, PermissionRequiredMixin):
     model = Product
     template_name = 'catalog/product_form.html'
     form_class = ProductAddForm
     success_url = reverse_lazy('catalog:index')
+    perms = ('catalog.edit_publication', 'catalog.edit_description', 'catalog.edit_categories')
+
+    def get_form_class(self):
+        if self.request.user.is_staff and self.request.user.has_perms(perm_list=self.perms) \
+                and not self.request.user.is_superuser:
+            return ProductAddForm
+        else:
+            if self.request.user != self.get_object().owner:
+                raise PermissionDenied
+            else:
+                return ProductAddForm
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
@@ -59,6 +72,7 @@ class ProductUpdateView(UpdateView):
     def form_valid(self, form):
         formset = self.get_context_data()['formset']
         self.object = form.save()
+        self.object.owner = self.request.user
         if formset.is_valid():
             formset.instance = self.object
             formset.save()
@@ -66,7 +80,10 @@ class ProductUpdateView(UpdateView):
         return super().form_valid(form)
 
 
-class ProudctDeleteView(DeleteView):
+class ProudctDeleteView(DeleteView, LoginRequiredMixin, PermissionRequiredMixin):
     model = Product
     template_name = 'catalog/product_confirm_delete.html'
     success_url = reverse_lazy('catalog:index')
+    permission_required = 'catalog.delete_product'
+
+
