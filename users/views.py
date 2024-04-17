86b35718820cabf8a7099013e0312b4b39012650
Danila Belov <danilabelov.adm@yandex.ru>
2024-04-17 21:03:06 +0300

    Изменён контроллер в приложении "пользователи"

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 import random
 
+from django.contrib.auth.views import PasswordResetConfirmView
 from django.core.mail import send_mail
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 
 from config import settings
@@ -28,7 +29,7 @@ class RegisterView(CreateView):
             subject='Верификация почты',
             message=f'Поздравляем с регистрацией на SkyStore \n'
                     f'Для подтверждения регистрации перейдите по ссылке: \n'
-                    f'http://127.0.0.1:8080/users/confirm/{user.token} \n'
+                    f'http://127.0.0.1:8000/users/confirm/{user.token} \n'
                     f'Если вы не причастны к регистрации игнорируйте это письмо.',
             from_email=settings.DEFAULT_FROM_EMAIL,
             recipient_list=[user.email]
@@ -37,12 +38,29 @@ class RegisterView(CreateView):
 
 
 def verify_view(request, token):
-    code = request.GET.get('code')
-    user = User.objects.get(token=code)
-    print(User.objects.get(token=code))
+    user = User.objects.get(token=token)
     user.is_verified = True
     user.save()
     return render(request, 'users/verify.html')
+
+
+def res_password(request):
+    new_password = ''
+    if request.method == 'POST':
+        email = request.POST['email']
+        user = get_object_or_404(User, email=email)
+        for i in range(10):
+            new_password += random.choice(CHARS)
+        send_mail(
+            subject='Смена пароля',
+            message=f'Ваш новый пароль {new_password}',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email]
+        )
+        user.set_password(new_password)
+        user.save()
+        return redirect(reverse('users:login'))
+    return render(request, 'users/reset_password.html')
 
 
 class ProfileView(UpdateView):
