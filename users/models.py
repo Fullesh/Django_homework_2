82c5d9ff82ffb4eed7e1714b9eecc51ac8746b55
Danila Belov <danilabelov.adm@yandex.ru>
2024-04-17 16:52:51 +0300

    Изменена модель пользователя

--- users/models.py
+++ users/models.py
@@ -11,6 +11,8 @@ class User(AbstractUser):
     phone = models.CharField(max_length=35, verbose_name='Телефон', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
     country = models.CharField(max_length=25, verbose_name='Страна', **NULLABLE)
+    is_verified = models.BooleanField(default=False, verbose_name='Подтверждён')
+    token = models.CharField(max_length=10, verbose_name='Токен', **NULLABLE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
