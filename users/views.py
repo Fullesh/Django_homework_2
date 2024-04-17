29297ab77a60fee8eecd651c3bab2f1dd4391c45
Danila Belov <danilabelov.adm@yandex.ru>
2024-04-17 14:28:29 +0300

    Добавлено приложение users

---
+++ users/views.py
@@ -0,0 +1,46 @@
+import random
+
+from django.core.mail import send_mail
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
+
+from config import settings
+from users.froms import UserRegisterForm, UserProfileForm
+from users.models import User
+CHARS = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        verified_pass = ''
+        for i in range(10):
+            verified_pass += random.choice(CHARS)
+        form.verified_pass = verified_pass
+        user = form.save()
+        user.verified_pass = verified_pass
+        send_mail(
+            subject='Верификация почты',
+            message=f'Поздравляем с регистрацией на SkyStore \n'
+                    f'Для подтверждения регистрации перейдите по ссылке: \n'
+                    f'http://127.0.0.1:8080/users/confirm?code={user.verified_pass} \n'
+                    f'Если вы не причастны к регистрации игнорируйте это письмо.',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[user.email]
+        )
+        return super().form_valid(form)
+
+
+class ProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
