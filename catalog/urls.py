1940ec47f803ffefc23573ec594cdbab31d3ed4c
Danila Belov <danilabelov.adm@yandex.ru>
2024-04-23 19:48:29 +0300

    Добавлены URL

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.views.decorators.cache import cache_page
 
-from catalog.views import indexListView, ProductDetailView, contactsPageView,\
-    ProductAddView, ProductUpdateView, ProudctDeleteView
+from catalog.views import indexListView, ProductDetailView, contactsPageView, \
+    ProductAddView, ProductUpdateView, ProudctDeleteView, CategoryListView, \
+    CategoryDetailView
 
 urlpatterns = [
     path('', indexListView.as_view(), name='index'),
@@ -11,5 +11,7 @@ urlpatterns = [
     path('products/<int:pk>', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
     path('products/edit/<int:pk>/', ProductUpdateView.as_view(), name='product_edit'),
     path('products/add/', ProductAddView.as_view(), name='product_add'),
-    path('products/delete/<int:pk>/', ProudctDeleteView.as_view(), name='product_delete')
+    path('products/delete/<int:pk>/', ProudctDeleteView.as_view(), name='product_delete'),
+    path('categories/', CategoryListView.as_view(), name='category_all'),
+    path('categories/view/<int:pk>/', CategoryDetailView.as_view(), name='category_detail')
 ]
